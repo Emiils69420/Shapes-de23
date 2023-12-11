e83bb88cef9c57b59036469d1b441d555cf3c888
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-12-11 14:45:17 +0200

    added

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import {useEffect} from "react";
+function App() {
+  useEffect(()=>{
+    async function getData(){
+      const response = await fetch("http://localhost:8080/");
+      const data = await response.json();
+      console.log(data)
+    }
+    getData();
+  },[]);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
